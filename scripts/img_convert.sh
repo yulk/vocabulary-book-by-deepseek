d1d34ea565220879de4e6c2836d549e4eebdd962
lh <whutluohui@gmail.com>
2025-03-15 21:41:15 +0800

    word imgs

--- scripts/img_convert.sh
+++ scripts/img_convert.sh
@@ -3,8 +3,9 @@
 PNG_FILES=`find ./result/word_imgs -name "*.png"`
 for file in $PNG_FILES
 do
+  echo "Processing: $file"
   convert $file -background white -quality 85 -flatten ${file}.tmp.jpg
-  convert ${file}.tmp.jpg -font Arial -pointsize 28 -fill "rgba(255,0,255,0.5)" -gravity southeast -annotate +20+10 "word.vxiaozhi.com" ${file%.png}.jpg
+  convert ${file}.tmp.jpg  imgs/vxiaozhi-watermark.png -gravity southeast -geometry +10+10 -composite ${file%.png}.jpg
 done
 
 #find ./result/word_imgs -name "*.tmp.jpg" -exec rm -v {} \; 
