aa12bda922030d77caa000ba495c01f26d84a793
vxiaozhi <whutluohui@gmail.com>
2025-03-15 22:40:59 +0800

    word imgs

--- scripts/img_convert.sh
+++ scripts/img_convert.sh
@@ -5,7 +5,7 @@ for file in $PNG_FILES
 do
   echo "Processing: $file"
   convert $file -background white -quality 85 -flatten ${file}.tmp.jpg
-  convert ${file}.tmp.jpg  imgs/vxiaozhi-watermark.png -gravity southeast -geometry +10+10 -composite ${file%.png}.jpg
+  convert ${file}.tmp.jpg  imgs/vxiaozhi-watermark2.png -gravity southeast -geometry +10+10 -composite ${file%.png}.jpg
 done
 
 #find ./result/word_imgs -name "*.tmp.jpg" -exec rm -v {} \; 
